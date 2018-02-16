cacc0a7dc92e560126e20dad6c7613de3910c3c7
julia <julyettka@gmail.com>
2018-02-16 21:20:47 +0200

    style: Deck info

--- components/DeckInfo.js
+++ components/DeckInfo.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native'
-import { white } from '../utils/colors'
+import { white, primaryDark, primaryLight, primary, primaryText, secondaryText } from '../utils/colors'
 
 function AddCardBtn ({ onPress }) {
 	return (
 			<TouchableOpacity
+			style={styles.addCardBtn}
 			onPress= {onPress}>
-                <Text> Add Card </Text>
+                <Text style={{color: white}}> Add Card </Text>
             </TouchableOpacity>
 		)
 }
@@ -14,8 +15,9 @@ function AddCardBtn ({ onPress }) {
 function StartQuizBtn ({ onPress }) {
 	return (
 			<TouchableOpacity
+			style={styles.quizBtn}
 			onPress= {onPress}>
-                <Text> Start Quiz </Text>
+                <Text style={{color: white}}> Start Quiz </Text>
             </TouchableOpacity>
 		)
 }
@@ -29,12 +31,14 @@ export default class DeckInfo extends Component {
 	}
 	render () {
 		const { title, questions } = this.props.navigation.state.params
-		return <View style={styles.container}>
-                <Text> {title} </Text>
-                <Text> {questions.length}{questions.length > 1 ? ` cards` : ` card` }</Text>
-                <AddCardBtn onPress={this.AddCardBtn}/>
-                <StartQuizBtn onPress={this.StartQuizBtn}/>
-            </View>
+		return (
+			<View style={styles.container}>
+	        	<Text style={styles.title}> {title} </Text>
+	            <Text style={styles.info}>{questions.length}{questions.length > 1 ? ` cards` : ` card` }</Text>
+	            <AddCardBtn onPress={this.AddCardBtn}/>
+	            <StartQuizBtn onPress={this.StartQuizBtn}/>
+	        </View>
+			)
     }
 }
 
@@ -43,5 +47,40 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-	}
+	},
+	title: {
+    	fontSize: 28,
+    	color: primaryText,
+		marginTop: 50,
+    },
+    info: {
+    	fontSize: 16,
+    	color: secondaryText,
+    	marginBottom: 140,
+    },
+    addCardBtn: {
+    	backgroundColor: primary,
+	    padding: 10,
+	    paddingLeft: 40,
+	    paddingRight: 40,
+	    height: 50,
+	    borderRadius: 2,
+	    justifyContent: 'center',
+	    alignItems: 'center',
+	    marginTop: 10,
+	    marginBottom: 10,
+    },
+    quizBtn: {
+    	backgroundColor: primaryDark,
+	    padding: 10,
+	    paddingLeft: 40,
+	    paddingRight: 40,
+	    height: 50,
+	    borderRadius: 2,
+	    justifyContent: 'center',
+	    alignItems: 'center',
+	    marginTop: 10,
+	    marginBottom: 10,
+    }
+
 })
